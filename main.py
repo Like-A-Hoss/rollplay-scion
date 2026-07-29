c33beccd4036fc7ba7930c9dee225eb526b69d8a
Nathan Hoskins <hapleswithdice@gmail.com>
2026-07-29 13:29:23 -0500

    Setting up bot

---
+++ main.py
@@ -0,0 +1,25 @@
+import nextcord
+from nextcord.ext import commands
+""" from settings.py import SECRET_KEY as SECRET_KEY
+from settings import TESTING_SERVER """
+import cogs.dice
+import cogs.embed_message_maker
+
+
+
+
+
+
+intents = nextcord.Intents.default()
+intents.members = True
+intents.message_content = True
+
+init_list = []
+
+
+
+client = commands.Bot(intents = intents)
+
+
+
+client.run(SECRET_KEY)
---
+++ settings.py
@@ -0,0 +1,29 @@
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+def get_param(name, default=None):
+    value = os.getenv(name)
+    if value:
+        return value
+
+    try:
+        import boto3
+
+        ssm = boto3.client('ssm', region_name='us-east-2')
+        return ssm.get_parameter(
+            Name=f'/rollplay-scion/{name}',
+            WithDecryption=True
+        )['Parameter']['Value']
+    except Exception:
+        if default is not None:
+            return default
+        raise
+
+
+# Load config from .env locally, with AWS Parameter Store as a fallback.
+SECRET_KEY = get_param('SECRET_KEY', '')
+TESTING_SERVER = get_param('TESTING_SERVER', '')
