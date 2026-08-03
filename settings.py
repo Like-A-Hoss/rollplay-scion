958ed1c3bb9a80218e2ee929ae1c9ae68acc9af4
Nathan Hoskins <hapleswithdice@gmail.com>
2026-08-03 15:05:55 -0500

    quick fix to make it AWS compliant

--- settings.py
+++ settings.py
@@ -1,30 +1,13 @@
-import os
-from dotenv import load_dotenv
+import boto3
 
-load_dotenv()
+ssm = boto3.client('ssm', region_name='us-east-2')
 
+def get_parameter(name):
+    return ssm.get_parameter(
+        Name=f"/rollplay-scion/{name}",
+        with_decryption=True)['Parameter']['Value']
+# Fetch bot token
+SECRET_KEY = get_parameter('SECRET_KEY')
 
-""" def get_param(name, default=None):
-    value = os.getenv(name)
-    if value:
-        return value
-
-    try:
-        import boto3
-
-        ssm = boto3.client('ssm', region_name='us-east-2')
-        return ssm.get_parameter(
-            Name=f'/rollplay-scion/{name}',
-            WithDecryption=True
-        )['Parameter']['Value']
-    except Exception:
-        if default is not None:
-            return default
-        raise
-
-
-# Load config from .env locally, with AWS Parameter Store as a fallback.
-SECRET_KEY = get_param('SECRET_KEY', '')
-TESTING_SERVER = get_param('TESTING_SERVER', '') """
-SECRET_KEY = os.getenv('SECRET_KEY', '')
-TESTING_SERVER = os.getenv('TESTING_SERVER', '')
+# Fetch test server ID
+TESTING_SERVER = get_parameter('TESTING_SERVER')
