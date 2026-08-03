ae6ad30230e8c7aa2210f8216f7694930f635a9e
Nathan Hoskins <hapleswithdice@gmail.com>
2026-08-03 15:20:35 -0500

    Making good with AWS

--- settings.py
+++ settings.py
@@ -5,7 +5,8 @@ ssm = boto3.client('ssm', region_name='us-east-2')
 def get_parameter(name):
     return ssm.get_parameter(
         Name=f"/rollplay-scion/{name}",
-        with_decryption=True)['Parameter']['Value']
+        WithDecryption=True
+    )['Parameter']['Value']
 # Fetch bot token
 SECRET_KEY = get_parameter('SECRET_KEY')
 
